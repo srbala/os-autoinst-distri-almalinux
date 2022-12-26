9f2c96d491bc074da5950aa8001eacb763c074ab
Bala Raman <srbala@gmail.com>
2022-12-26 05:55:07 -0500

    Needles update
    Signed-off-by: Bala Raman <srbala@gmail.com>

--- tests/disk_custom_resize_lvm.pm
+++ tests/disk_custom_resize_lvm.pm
@@ -62,7 +62,7 @@ sub run {
     assert_and_click "anaconda_part_update_settings";
     # give it a second or two to update
     wait_still_screen 2;
-    # Check that the partition has been resized for 13GiB
+    # Check that the partition has been resized for 11GiB
     assert_screen "device_root_resized_thirteen";
 
     # Add new /home partition into the emptied space.
